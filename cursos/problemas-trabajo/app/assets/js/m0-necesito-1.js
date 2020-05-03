9c82ed4efb326da9d94d860928747c6eeaeb646e
Rodrigo Bilbao <rbilbao98@gmail.com>
2020-05-03 17:13:33 +0200

    Modulo0 completo y base de Temario

---
+++ cursos/problemas-trabajo/app/assets/js/m0-necesito-1.js
@@ -0,0 +1,37 @@
+// TRIANGULo
+$('button[name="valores"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("tri");
+
+  img.src = "../../assets/images/course/modulo0/valores.jpg"
+});
+
+$('button[name="recomendaciones"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("tri");
+
+  img.src = "../../assets/images/course/modulo0/recomendaciones.jpg"
+});
+
+$('button[name="reglas"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("tri");
+
+  img.src = "../../assets/images/course/modulo0/reglas.jpg"
+});
+
+
+// RECTANGULO
+$('button[name="lideres"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("pri");
+
+  img.src = "../../assets/images/course/modulo0/lideres.gif"
+});
+
+$('button[name="equipo"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("pri");
+
+  img.src = "../../assets/images/course/modulo0/equipo.gif"
+});
+$('button[name="innova"][type="button"]').on('click', function(event) {
+  var img = document.getElementById("pri");
+
+  img.src = "../../assets/images/course/modulo0/innova.gif"
+});
--- cursos/problemas-trabajo/app/assets/js/quizes/quiz-m0.js
+++ cursos/problemas-trabajo/app/assets/js/quizes/quiz-m0.js
@@ -8,7 +8,8 @@ $('input[name="verificar"][type="button"]').on('click', function(event) {
     }
   }
 
-  alert("Has obtenido una puntuación de " + ans);
-  //location.replace("modulo1-1.html");
+  alert("Has obtenido una puntuación de " + ans + "\n\n¡Felicitaciones! El completar esta valoración corroboras tu deseo de desarrollar el desempeño de tus propias metas. Tu puntaje refleja en dónde te encuentras en este momento. En todos los casos hay siempre oportunidad de poder construir tu capacidad de desempeño.");
+  alert("Este curso te ayudará significativamente a desarrollar tus conocimientos y habilidades de comunicación interpersonal. Es momento de comenzar.");
+  location.replace("modulo1-1.html");
   event.preventDefault();
 });
